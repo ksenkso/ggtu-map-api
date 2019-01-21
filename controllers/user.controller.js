64b414052a306d728f563badf83f7e627a778fb5
ksenkso <yazunya@gmail.com>
2019-01-21 22:06:52 +0300

    Added route to get token info. `create` spliced into two routes: `create` and `register`. The route `register` can be used publicly, `create` requires authentication and `root` role. `create` route allows to create users  with any role.  `checkAuth` route added.

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,9 @@
+const jwt = require("jsonwebtoken");
 const authService = require('../services/auth.service');
 const {ReS, handleError} = require('../services/util.service');
 const pe = require('parse-error');
+const {User} = require('../models');
+const CONFIG = require('../config/config');
 // const debug = require('debug')('Auth');
 /**
  *
@@ -10,19 +13,25 @@ const pe = require('parse-error');
  * @return {Promise<Model>}
  */
 const create = async function (req, res, next) {
-    const {login, password} = req.body;
+    let {login, password, role} = req.body;
     const errors = [];
     if (!login) {
-        errors.push(new Error('Please enter your login'));
+        errors.push(new Error('Please enter login'));
     }
     if (!password) {
-        errors.push(new Error('Please enter your password'));
+        errors.push(new Error('Please enter password'));
+    }
+    if (
+        role && (req.user || req.user.role !== 'root') ||
+        !role
+    ) {
+        role = 'consumer';
     }
     if (!errors.length) {
         try {
-            const user = await authService.createUser(login, password);
+            const user = await authService.createUser(login, password, role);
+            user.generateJWT();
             const userInfo = user.getClean();
-            userInfo.token = user.getJWT();
             return ReS(res, userInfo, 201);
         } catch (e) {
             next(e);
@@ -33,6 +42,62 @@ const create = async function (req, res, next) {
 
 };
 module.exports.create = create;
+
+const tokenInfo = async function (req, res, next) {
+    const token = req.get('Authorization').substring(7);
+    if (token) {
+        try {
+            const info = await new Promise((resolve, reject) => {
+                jwt.verify(token, CONFIG.jwt_encryption, (error, decoded) => {
+                    if (decoded) {
+                        resolve(decoded);
+                    } else {
+                        console.log(error);
+                        reject(error);
+                    }
+                });
+            });
+            if (info) {
+                info.text = token;
+                return ReS(res, info, 200);
+            } else {
+                return next(new Error('wtf'))
+            }
+        } catch (e) {
+            return next(e);
+        }
+    } else {
+        return next()
+
+    }
+
+};
+module.exports.tokenInfo = tokenInfo;
+
+const register = async function (req, res, next) {
+    const {login, password} = req.body;
+    const role = 'consumer';
+    const errors = [];
+    if (!login) {
+        errors.push(new Error('Please enter your login'));
+    }
+    if (!password) {
+        errors.push(new Error('Please enter your password'));
+    }
+    if (!errors.length) {
+        try {
+            const user = await authService.createUser(login, password, role);
+            user.generateJWT();
+            const userInfo = user.getClean();
+            return ReS(res, userInfo, 201);
+        } catch (e) {
+            next(e);
+        }
+    } else {
+        return next(errors);
+    }
+};
+module.exports.register = register;
 
 const get = async function (req, res) {
     let user = req.user;
@@ -54,7 +119,7 @@ const remove = async function (req, res, next) {
     const user = req.user, id = user.id;
     try {
         await user.destroy();
-        return ReS(res, {id}, 204);
+        return ReS(res, {id}, 200);
     } catch (e) {
         next(e);
     }
@@ -65,14 +130,43 @@ module.exports.remove = remove;
 const login = async function (req, res, next) {
     try {
         const user = await authService.authUser(req.body);
-        return ReS(res, {token: user.getJWT(), user: user.getClean()});
+        user.generateJWT();
+        return ReS(res, user.getClean());
     } catch (e) {
         const err = pe(e);
-        err.status = 422;
-        handleError(e, next);
+        err.status = 401;
+        handleError(err, next);
     }
 };
 module.exports.login = login;
+
+
+const checkAuth = async function (req, res, next) {
+    /**
+     * 1. Check if token is expired
+     * 2. Check if token is signed properly
+     * 3. Check if token contains valid user id
+     */
+    try {
+        const decoded = jwt.verify(req.query.token, CONFIG.jwt_encryption);
+        if (decoded) {
+            const ok = !!await User.count({where: {id: decoded.user_id}});
+            console.log(ok);
+            if (!ok) {
+                const error = new Error('Такого пользователя не существует.');
+                error.status = 401;
+                handleError(error, next);
+            } else {
+                ReS(res, {ok});
+            }
+        }
+    } catch (e) {
+        const err = pe(e);
+        err.status = 401;
+        handleError(err, next);
+    }
+};
+module.exports.checkAuth = checkAuth;
 
 const me = async function (req, res) {
     return ReS(res, {user: req.user.getClean()});
