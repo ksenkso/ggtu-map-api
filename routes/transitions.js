6ca5aa72b72c392e03c6a8e52566a3e587466b61
ksenkso <yazunya@gmail.com>
2019-02-03 21:58:32 +0300

    `addView` route

--- routes/transitions.js
+++ routes/transitions.js
@@ -12,9 +12,10 @@ const checkTransitionPermissions = checkAccess({
 });
 
 module.exports = (router) => {
-    router.post('/transitions', auth, TransitionsController.create);                                                                                           // C
-    router.get('/transitions', auth, querying.enableRelations, querying.enableLimits, TransitionsController.getAll);                                                                                           // C
-    router.get('/transitions/:id', auth, querying.enableRelations, querying.enableLimits, checkTransitionPermissions, TransitionsController.get);                                            // R
-    router.patch('/transitions/:id', auth, checkTransitionPermissions, TransitionsController.update);                                            // U
+    router.post('/transitions', auth, TransitionsController.create);
+    router.get('/transitions', auth, querying.enableRelations, querying.enableLimits, TransitionsController.getAll);
+    router.post('/transitions/:id', auth, TransitionsController.addView);
+    router.get('/transitions/:id', auth, querying.enableRelations, querying.enableLimits, checkTransitionPermissions, TransitionsController.get);
+    router.patch('/transitions/:id', auth, checkTransitionPermissions, TransitionsController.update);
     router.delete('/transitions/:id', auth, checkTransitionPermissions, TransitionsController.remove);
 };
