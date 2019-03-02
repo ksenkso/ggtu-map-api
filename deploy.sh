4d1da79739838b436da830680f1665d09885af87
ksenkso <yazunya@gmail.com>
2019-03-02 20:43:37 +0300

    run npm

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,11 @@
 git config --global push.default simple # we only want to push one branch — master
 # specify the repo on the live server as a remote repo, and name it 'production'
 # <user> here is the separate user you created for deploying
+git remote add old https://github.com/ksenkso/ggtu-map-api.git
 git remote add production ssh://root@37.140.199.148/app
+git fetch --unshallow old
 git push production master # push our updates
-ssh root@37.140.199.148 'npm install && npm run start'
+ssh root@37.140.199.148 << EOF
+cd /app
+npm install
+EOF
