7b56e99be36b7677898f4ded6d9921701e181f96
ksenkso <yazunya@gmail.com>
2019-03-26 07:08:48 +0300

    helpers for place props

--- models/place-props.model.js
+++ models/place-props.model.js
@@ -12,6 +12,20 @@ module.exports = (sequelize, DataTypes) => {
 
     PlaceProps.associate = function (models) {
         PlaceProps.belongsTo(models.Place);
+        PlaceProps.WC = {
+            MALE: 0,
+            FEMALE: 1,
+            getType(name) {
+                return name === 'мужской' ? this.MALE : this.FEMALE;
+            }
+        };
+        PlaceProps.BUILDING = {
+            STUDY: 'study',
+            LIVE: 'live',
+            getType(name = 'корпус') {
+                return name === 'корпус' ? this.STUDY : this.LIVE;
+            }
+        };
     };
 
     return PlaceProps;
